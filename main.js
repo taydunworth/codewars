9ea4e513f98515b18db98e9f64c1c3eb68601dd2
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-13 17:15:53 -0400

    Added solution for Sum Strings as Numbers

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("test");
+// Title: Sum Strings as Numbers
+// Level: 4 kyu
+// URL: https://www.codewars.com/kata/5324945e2ece5e1f32000370/train/javascript
+function sumStrings(a,b) {
+  let sum = parseInt(a) + parseInt(b)
+  sum = sum.toString();
+  return sum;
+}
