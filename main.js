f36c111e739b266b2fac8e08f0a064647ae63e9c
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-13 17:41:43 -0400

    Fixed issue on Sum Strings as Numbers for if a value is an empty string

--- main.js
+++ main.js
@@ -2,7 +2,20 @@
 // Level: 4 kyu
 // URL: https://www.codewars.com/kata/5324945e2ece5e1f32000370/train/javascript
 function sumStrings(a,b) {
+  if (a === "") {
+    a = 0;
+    let sum = a + parseInt(b)
+    sum = sum.toString();
+    return sum;
+  } else if (b === "") {
+      b = 0;
+      let sum = parseInt(a) + b
+      sum = sum.toString();
+      return sum;
+  }
   let sum = parseInt(a) + parseInt(b)
   sum = sum.toString();
   return sum;
 }
+
+console.log(sumStrings("712569312664357328695151392", "8100824045303269669937"));
