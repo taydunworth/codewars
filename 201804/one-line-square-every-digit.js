c68a14e755f54a804116e72d3464f3ee6120daa9
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-20 21:39:51 -0400

    Logic is right for One Line Square Every Digit but is 53 characters long instead of 36

---
+++ 201804/one-line-square-every-digit.js
@@ -0,0 +1,7 @@
+// Title: One Line: Square Every Digit
+// Level: 6 kyu
+// Date Completed: In Progress
+// URL: https://www.codewars.com/kata/one-line-task-square-every-digit/train/javascript
+// Description: Inspired by Square Every Digit (and by the inimitable myjinxin2015's many clever one-line kata), your goal here is precisely the same (to square every digit in the given number), in 36 or fewer characters (JavaScript), or 48 or fewer in Ruby.
+
+sd=x=>parseInt(`${x}`.split("").map(y=>y*y))
