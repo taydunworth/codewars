741049b3252e5192ff77a87365d0b6d024167e80
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-23 10:42:41 -0400

    Reversed the two strings and mirrored the second. Only need to figure out inverting the cases

--- 201804/string-reversing-changing-case-etc.js
+++ 201804/string-reversing-changing-case-etc.js
@@ -5,5 +5,9 @@
 // Description: Given 2 string parameters, show a concatenation of: the reverse of the 2nd string with inverted case; e.g Fish -> HSIf; a separator in between both strings: @@@; the 1st string reversed with inverted case and then mirrored; e.g Water -> RETAwwATER
 
 function reverseAndMirror(s1,s2) {
-  return "";
+  reversedS2 = s2.split("").reverse().join("");
+  reversedS1 = s1.split("").reverse().join("") + s1
+  return `${reversedS2}@@@${reversedS1}`
 }
+
+console.log(reverseAndMirror("FiZZ","buZZ"));
