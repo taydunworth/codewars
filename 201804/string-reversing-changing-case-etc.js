1c1bcce060b7a6616728f24f4903998d6c203f1c
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-23 10:35:25 -0400

    Added starter files for Sring Reversing, Changing Case, etc.

---
+++ 201804/string-reversing-changing-case-etc.js
@@ -0,0 +1,9 @@
+// Title: String Reversing, Changing Case, etc.
+// Level: 7 kyu
+// Date Completed: In Progress
+// URL: https://www.codewars.com/kata/string-reversing-changing-case-etc/train/javascript
+// Description: Given 2 string parameters, show a concatenation of: the reverse of the 2nd string with inverted case; e.g Fish -> HSIf; a separator in between both strings: @@@; the 1st string reversed with inverted case and then mirrored; e.g Water -> RETAwwATER
+
+function reverseAndMirror(s1,s2) {
+  return "";
+}
