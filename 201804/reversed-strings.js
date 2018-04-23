15515097e6536e2ae6819131ef3d37153bfdc595
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-23 10:29:58 -0400

    Added solution for Reversed Strings

---
+++ 201804/reversed-strings.js
@@ -0,0 +1,9 @@
+// Title: Reversed Strings
+// Level: 8 kyu
+// Date Completed: 4/23/18
+// URL: https://www.codewars.com/kata/reversed-strings/train/javascript
+// Description: Complete the solution so that it reverses the string value passed into it.
+
+function solution(str){
+  return str.split("").reverse().join("")
+}
