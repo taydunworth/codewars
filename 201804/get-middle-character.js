a9007190990f35ac7811b0040c8abcfcb281fd8e
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-23 16:54:30 -0400

    Added solution for Get Middle Character

--- 201804/get-middle-character.js
+++ 201804/get-middle-character.js
@@ -1,8 +1,9 @@
 // Title: Get the Middle Character
 // Level: 7 kyu
-// Date Completed: In Progress
+// Date Completed: 4/23/18
 // URL: https://www.codewars.com/kata/get-the-middle-character/train/javascript
 // Description: You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
 function getMiddle(s) {
+  return s.length % 2 == 0 ? s[s.length / 2 - 1] + s[s.length / 2] : s[s.length / 2 | 0]
 }
