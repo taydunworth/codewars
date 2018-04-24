f674a0d6e5a7f8a5fd46711b1aebe41588126562
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-24 16:38:03 -0400

    Finally got One Light Square Every Digit Solved with outside help. Changed arrayFrom() to a spread within an array and backticks on join to cut down on characters

--- 201804/one-line-square-every-digit.js
+++ 201804/one-line-square-every-digit.js
@@ -1,7 +1,7 @@
 // Title: One Line: Square Every Digit
 // Level: 6 kyu
-// Date Completed: In Progress
+// Date Completed: 4/24/18
 // URL: https://www.codewars.com/kata/one-line-task-square-every-digit/train/javascript
 // Description: Inspired by Square Every Digit (and by the inimitable myjinxin2015's many clever one-line kata), your goal here is precisely the same (to square every digit in the given number), in 36 or fewer characters (JavaScript), or 48 or fewer in Ruby.
 
-sd=x=>+Array.from(""+x,y=>y*y).join("")
+sd=x=>+[...""+x].map(y=>y*y).join``;
