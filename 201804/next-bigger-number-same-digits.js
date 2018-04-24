a36d32180a3e438608d1cebd404c5b31636ab41f
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-24 11:53:05 -0400

    Added starter code for Next Bigger Number with the Same Digits

---
+++ 201804/next-bigger-number-same-digits.js
@@ -0,0 +1,9 @@
+// Title: Next bigger number with the same digits
+// Level: 4 kyu
+// Date Completed: In Progress
+// URL: https://www.codewars.com/kata/next-bigger-number-with-the-same-digits/train/javascript
+// Description: You have to create a function that takes a positive integer number and returns the next bigger number formed by the same digits. If no bigger number can be composed using those digits, return -1:
+
+function nextBigger(n){
+  //your code here
+}
