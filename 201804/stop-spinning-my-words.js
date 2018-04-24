c842fa7b5f330465363d74b38d36869ef6a9822f
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-24 14:12:29 -0400

    Added solution for Convert a String to a Number

--- 201804/stop-spinning-my-words.js
+++ 201804/stop-spinning-my-words.js
@@ -1,6 +1,6 @@
 // Title: Stop gninnipS My sdroW!
 // Level: 6 kyu
-// Date Completed: In Progress
+// Date Completed: 4/24/18
 // URL: https://www.codewars.com/kata/stop-gninnips-my-sdrow/train/javascript
 // Description: Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
