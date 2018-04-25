f202b5a511b8e2502a9d345aff6e93b4f173dc34
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-24 21:35:20 -0400

    Added solution for String Repeat

---
+++ 201804/string-repeat.js
@@ -0,0 +1,9 @@
+// Title: String repeat
+// Level: 8 kyu
+// Date Completed: 4/24/18
+// URL: https://www.codewars.com/kata/string-repeat/train/javascript
+// Description: Write a function called repeatStr which repeats the given string string exactly n times.
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
