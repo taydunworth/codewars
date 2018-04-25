0a1145ce1e37c9e79369d4bdc9db9f27bc7850dd
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-24 21:11:32 -0400

    Fixed URL on If Else Ternary an added starter code for Reverse List Order

--- 201804/if-else-ternary.js
+++ 201804/if-else-ternary.js
@@ -1,7 +1,7 @@
 // Title: Training JS #7: if..else and ternary operator
 // Level: 8 kyu
 // Date Completed: 4/24/18
-// URL: https://www.codewars.com/kata/students-final-grade/train/javascript
+// URL: https://www.codewars.com/kata/training-js-number-7-if-dot-else-and-ternary-operator/train/javascript
 // Description: Complete function saleHotdogs/SaleHotDogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.
 
 // +---------------+-------------+
---
+++ 201804/reverse-list-order.js
@@ -0,0 +1,5 @@
+// Title: Reverse List Order
+// Level: 8 kyu
+// Date Completed: 4/24/18
+// URL: https://www.codewars.com/kata/reverse-list-order/train/javascript
+// Description: In this kata you will create a function that takes in a list and returns a list with the reverse order.
