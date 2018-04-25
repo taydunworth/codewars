4bbfd5d89caf5d249f1d9d82169db1f7af093bd9
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-24 20:58:26 -0400

    Added solution for Student's Final Grade

---
+++ 201804/students-grade.js
@@ -0,0 +1,14 @@
+// Title: Student's Final Grade
+// Level: 8 kyu
+// Date Completed: 4/24/18
+// URL: https://www.codewars.com/kata/students-final-grade/train/javascript
+// Description: Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects. This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+//This function should return a number (final grade). There are four types of final grades:
+// • 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// • 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// • 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// • 0, in other cases
+
+function finalGrade (exam, projects) {
+  return exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0
+}
