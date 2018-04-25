a449dad1518f577c91cf1c15075844240842ae2c
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-24 21:12:35 -0400

    Added solution for Reverse List Order

--- 201804/reverse-list-order.js
+++ 201804/reverse-list-order.js
@@ -3,3 +3,7 @@
 // Date Completed: 4/24/18
 // URL: https://www.codewars.com/kata/reverse-list-order/train/javascript
 // Description: In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+function reverseList(list) {
+  return list.reverse();
+}
