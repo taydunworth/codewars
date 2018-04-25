1dde8cef316011aa4bfe46ac3a81cfbea827efe6
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-24 21:54:54 -0400

    Added solution for Combine Strings Function

---
+++ 201804/combine-strings-function.js
@@ -0,0 +1,7 @@
+// Title: Grasshopper - Combine strings
+// Level: 8 kyu
+// Date Completed: 4/24/18
+// URL: https://www.codewars.com/kata/grasshopper-combine-strings/train/javascript
+// Description: Create a function named combineNames(combine_names in python, ruby) that accepts two parameters (first and last name). The function should return the full name.
+
+const combineNames = (a, b) => {return `${a} ${b}`;}
