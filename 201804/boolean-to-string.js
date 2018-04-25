2e84e6f5f6e2bc72a76c9291ea6c6610f00278d4
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-24 21:15:00 -0400

    Added solution for Boolean to String

---
+++ 201804/boolean-to-string.js
@@ -0,0 +1,8 @@
+// Title: Boolean to String
+// Level: 8 kyu
+// Date Completed: 4/24/18
+// URL: https://www.codewars.com/kata/convert-a-boolean-to-a-string/train/javascript
+
+function booleanToString(b){
+  return b.toString();
+}
