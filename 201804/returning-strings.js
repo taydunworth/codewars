d2afefddb09d6762f8a7cfc1b4fc59964410ca11
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-26 10:34:49 -0400

    Added solution for Returning Strings

---
+++ 201804/returning-strings.js
@@ -0,0 +1,9 @@
+// Title: Returning Strings
+// Level: 8 kyu
+// Date Completed: 4/26/18
+// URL: https://www.codewars.com/kata/returning-strings/train/javascript
+// Description: Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
