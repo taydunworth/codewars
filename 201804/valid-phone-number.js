a7bf9c61c2e924f0435d5154713e198bc7302577
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-26 14:51:41 -0400

    Updated regex to clean up the code and solved Valid Phone Number

--- 201804/valid-phone-number.js
+++ 201804/valid-phone-number.js
@@ -5,6 +5,6 @@
 // Description: Write a function that accepts a string, and returns true if it is in the form of a phone number.  Assume that any integer from 0-9 in any of the spots will produce a valid phone number. Only worry about the following format: (123) 456-7890 (don't forget the space after the close parentheses)
 
 function validPhoneNumber(phoneNumber){
-  let phoneRegex = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/
-  return phoneRegex.test(phoneNumber) && phoneNumber.length === 14 ? true : false
+  let phoneRegex = /^\(\d{3}\)\s{1}\d{3}-\d{4}$/
+  return phoneRegex.test(phoneNumber) ? true : false
 }
