d82ea45e4c5e4000219cf3ef059cec33d08ca12a
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-26 16:04:44 -0400

    Updated date completed in Valid Phone Number

--- 201804/valid-phone-number.js
+++ 201804/valid-phone-number.js
@@ -1,6 +1,6 @@
 // Title: Valid Phone Number
 // Level: 6 kyu
-// Date Completed: In Progress
+// Date Completed: 4/27/18
 // URL: https://www.codewars.com/kata/valid-phone-number/train/javascript
 // Description: Write a function that accepts a string, and returns true if it is in the form of a phone number.  Assume that any integer from 0-9 in any of the spots will produce a valid phone number. Only worry about the following format: (123) 456-7890 (don't forget the space after the close parentheses)
 
